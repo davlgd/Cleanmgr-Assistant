544bfe5fb848a9103ee9584ab0059f75c15c279d
David Legrand <david@nextinpact.com>
2017-08-25 15:36:25 +0200

    Première version publique de l'application

---
+++ Cleanmgr Assistant/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Cleanmgr_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbDrives = new System.Windows.Forms.ComboBox();
+            this.cbActions = new System.Windows.Forms.ComboBox();
+            this.btDrives = new System.Windows.Forms.Button();
+            this.btActions = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // cbDrives
+            // 
+            this.cbDrives.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDrives.FormattingEnabled = true;
+            this.cbDrives.Location = new System.Drawing.Point(12, 12);
+            this.cbDrives.Name = "cbDrives";
+            this.cbDrives.Size = new System.Drawing.Size(260, 21);
+            this.cbDrives.TabIndex = 2;
+            this.cbDrives.SelectedIndexChanged += new System.EventHandler(this.cbDrives_SelectedIndexChanged);
+            // 
+            // cbActions
+            // 
+            this.cbActions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbActions.FormattingEnabled = true;
+            this.cbActions.Items.AddRange(new object[] {
+            "Lancer avec les paramètres par défaut",
+            "Lancer et tout sélectionner (mode manuel)",
+            "Lancer et tout sélectionner (mode automatique)",
+            "----",
+            "Configurer le profil #42",
+            "Lancer le profil #42",
+            "----",
+            "Autoclean"});
+            this.cbActions.Location = new System.Drawing.Point(12, 39);
+            this.cbActions.Name = "cbActions";
+            this.cbActions.Size = new System.Drawing.Size(341, 21);
+            this.cbActions.TabIndex = 3;
+            this.cbActions.SelectedIndexChanged += new System.EventHandler(this.cbActions_SelectedIndexChanged);
+            // 
+            // btDrives
+            // 
+            this.btDrives.Location = new System.Drawing.Point(278, 10);
+            this.btDrives.Name = "btDrives";
+            this.btDrives.Size = new System.Drawing.Size(75, 23);
+            this.btDrives.TabIndex = 4;
+            this.btDrives.Text = "Rafraîchir";
+            this.btDrives.UseVisualStyleBackColor = true;
+            this.btDrives.Click += new System.EventHandler(this.btDrives_Click);
+            // 
+            // btActions
+            // 
+            this.btActions.Location = new System.Drawing.Point(278, 66);
+            this.btActions.Name = "btActions";
+            this.btActions.Size = new System.Drawing.Size(75, 23);
+            this.btActions.TabIndex = 5;
+            this.btActions.Text = "Go !";
+            this.btActions.UseVisualStyleBackColor = true;
+            this.btActions.Click += new System.EventHandler(this.btActions_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 76);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(166, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Legrand David - Next INpact.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 100);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btActions);
+            this.Controls.Add(this.btDrives);
+            this.Controls.Add(this.cbActions);
+            this.Controls.Add(this.cbDrives);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cbDrives;
+        private System.Windows.Forms.ComboBox cbActions;
+        private System.Windows.Forms.Button btDrives;
+        private System.Windows.Forms.Button btActions;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Cleanmgr Assistant/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Cleanmgr_Assistant
+{
+    public partial class Form1 : Form
+    {
+        private string selectedDriveLetter = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = string.Format("{0} v{1}",
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            refreshFixedDisks();
+            btActionCheckStatus();
+        }
+
+        private void refreshFixedDisks()
+        {
+            cbDrives.Items.Clear();
+
+            DriveInfo[] di = DriveInfo.GetDrives();
+            foreach (var d in di)
+            {
+                if (d.DriveType == DriveType.Fixed) cbDrives.Items.Add(string.Format("{0} ({1}) : {2} Go libres sur {3} Go{4}",
+                    d.Name,
+                    d.DriveFormat,
+                    d.AvailableFreeSpace / 1024 / 1024 / 1024,
+                    d.TotalSize / 1024 / 1024 / 1024,
+                    Environment.NewLine));
+            }
+
+            if (cbDrives.Items.Count > 0) cbDrives.SelectedIndex = 0;
+            else
+            {
+                MessageBox.Show("Aucun disque détecté, l'application va être fermée", "Erreur - Disque",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+        }
+
+        private void btActions_Click(object sender, EventArgs e)
+        {
+            string args = string.Empty;
+
+            switch (cbActions.SelectedIndex)
+            {
+                case 0:
+                    args = "/D " + selectedDriveLetter;
+                    break;
+
+                case 1:
+                    args = "/LOWDISK /D " + selectedDriveLetter;
+                    break;
+
+                case 2:
+                    args = "/VERYLOWDISK /D " + selectedDriveLetter;
+                    break;
+
+                case 4:
+                    args = "/sageset:42";
+                    break;
+
+                case 5:
+                    args = "/sagerun:42";
+                    break;
+
+                case 7:
+                    args = "/autoclean /D " + selectedDriveLetter;
+                    break;
+            }
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cleanmgr.exe";
+            p.StartInfo.Arguments = args;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+        }
+
+        private void btActionCheckStatus()
+        {
+            btActions.Enabled = (cbActions.SelectedIndex != 3 && cbActions.SelectedIndex != 6 && cbActions.SelectedIndex > -1) ? true : false;
+        }
+
+        private void btDrives_Click(object sender, EventArgs e)
+        {
+            refreshFixedDisks();
+        }
+
+        private void cbActions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btActionCheckStatus();
+        }
+
+        private void cbDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedDriveLetter = cbDrives.SelectedItem.ToString().Split('\\')[0];
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(@"https://www.nextinpact.com/news/104401-assistant-nettoyage-windows-cleanmgr-exe-supprimez-facilement-fichiers-inutiles.htm");
+        }
+    }
+}
